9bf47f4f88d6e3ae632e5805c4f45cd3e109e23f
an.phamhung@gmail.com <an.phamhung@gmail.com>
2016-09-19 21:51:20 +0700

    fix crash

--- app/src/main/java/phamhungan/com/phonetestv3/ui/fragment/NullFragment.java
+++ app/src/main/java/phamhungan/com/phonetestv3/ui/fragment/NullFragment.java
@@ -1,5 +1,6 @@
 package phamhungan.com.phonetestv3.ui.fragment;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -11,16 +12,11 @@ import phamhungan.com.phonetestv3.R;
 /**
  * Created by MrAn PC on 22-Jan-16.
  */
-public class NullFragment extends BaseFragment {
+public class NullFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = super.onCreateView(inflater,container,savedInstanceState);
-        return view;
-    }
-
-    @Override
-    protected int getRootLayout() {
-        return R.layout.layout_null;
+        super.onCreateView(inflater,container,savedInstanceState);
+        return inflater.inflate(R.layout.layout_null, container, false);
     }
 }
